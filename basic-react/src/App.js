7990e391a13ed0b5759a60bd8a959b8428afa978
Varun Dhawan <vddhawan9@gmail.com>
2026-06-18 23:55:27 +0530

    Created basic react app via create react-app and understands the flow of files

---
+++ basic-react/src/App.js
@@ -0,0 +1,11 @@
+import Chai from "./Random";
+
+function App() {
+  return (
+    <>
+      <Chai />
+    </>
+  );
+}
+
+export default App;
---
+++ basic-react/src/Random.js
@@ -0,0 +1,4 @@
+function Chai() {
+  return <h2>Chai is cooking</h2>;
+}
+export default Chai;
