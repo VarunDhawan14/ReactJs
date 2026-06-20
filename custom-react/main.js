5a6752b87b9d8f7e2982de9d96dc74e3e7534aeb
Varun Dhawan <vddhawan9@gmail.com>
2026-06-20 23:34:26 +0530

    just added some data in main.js file

--- custom-react/main.js
+++ custom-react/main.js
@@ -1,1 +1,12 @@
+const { Children } = require("react");
+
 const domElement = document.createElement("div");
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "blank",
+  },
+  Children: "Click me to visit google",
+};
